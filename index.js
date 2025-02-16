1a23295af4dc6c9ca71796ad67dc934bb688b82a
Aathira Jayakumar <aswinchembath@gmail.com>
2025-02-16 14:37:02 +0530

    added cli option

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
+#!/usr/bin/env node
 import JSONReporter from './lib/JSONReporter.js';
 import HTMLReportGenerator from './lib/HTMLReportGenerator.js';
 
 export { JSONReporter, HTMLReportGenerator };
+
+// If this file is run directly from the CLI, process command-line arguments.
+if (import.meta.url === new URL(process.argv[1], 'file://').href) {
+  // For example, support a command "generate-html" that takes an input folder and an output file:
+  const [ , , command, ...args] = process.argv;
+  if (command === 'generate-html') {
+    const [inputFolder, outputFile] = args;
+    if (!inputFolder || !outputFile) {
+      console.error('Usage: generate-html <inputFolder> <outputFile>');
+      process.exit(1);
+    }
+    const generator = new HTMLReportGenerator(outputFile);
+    generator.convertJSONFolderToHTML(inputFolder)
+      .then(() => console.log('HTML report generated successfully.'))
+      .catch(err => console.error('Error generating HTML report:', err));
+  } else {
+    console.error('Usage: node index.js generate-html <inputFolder> <outputFile>');
+    process.exit(1);
+  }
+}
