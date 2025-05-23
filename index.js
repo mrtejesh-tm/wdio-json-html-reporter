f945f59a304140922194cc7c9c536a12ae72745b
aswinchembath <aswinchembath@gmail.com>
2025-05-23 13:09:11 +0000

    e5-6 fix

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 #!/usr/bin/env node
-import path from 'path';
-import { fileURLToPath } from 'url';
-import JSONReporter from './lib/JSONReporter.js';
-import HTMLReportGenerator from './lib/HTMLReportGenerator.js';
-
-export { JSONReporter, HTMLReportGenerator };
+const path = require('path');
+const JSONReporter = require('./lib/JSONReporter');
+const HTMLReportGenerator = require('./lib/HTMLReportGenerator');
+module.exports = { JSONReporter, HTMLReportGenerator };
 
 /**
  * Prints the usage message and exits.
